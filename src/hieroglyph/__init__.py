8cb4d9a4b5e891792110f223ecf0eb3dc204eca0
Nathan Yergler <nathan@yergler.net>
2012-03-10 22:50:35 -0800

    Move extension registration out of the builders module.

--- src/hieroglyph/__init__.py
+++ src/hieroglyph/__init__.py
@@ -1,1 +1,11 @@
-from builder import setup
+import builder
+
+def setup(app):
+
+    app.add_builder(builder.SlideBuilder)
+    app.add_builder(builder.DirectorySlideBuilder)
+
+    app.add_config_value('slide_theme', 'slides', 'html')
+    app.add_config_value('slide_levels', 3, 'html')
+    app.add_config_value('slide_theme_options', {}, 'html')
+    app.add_config_value('slide_theme_path', [], 'html')
--- src/hieroglyph/builder.py
+++ src/hieroglyph/builder.py
@@ -102,13 +102,3 @@ class DirectorySlideBuilder(AbstractSlideBuilder, DirectoryHTMLBuilder):
 class SlideBuilder(AbstractSlideBuilder, StandaloneHTMLBuilder):
 
     name = 'html5slides'
-
-def setup(app):
-
-    app.add_builder(SlideBuilder)
-    app.add_builder(DirectorySlideBuilder)
-
-    app.add_config_value('slide_theme', 'slides', 'html')
-    app.add_config_value('slide_levels', 3, 'html')
-    app.add_config_value('slide_theme_options', {}, 'html')
-    app.add_config_value('slide_theme_path', [], 'html')
