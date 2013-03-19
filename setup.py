eb245a4215f44c61e0c6e9d1eb34f6561253b678
Nathan Yergler <nathan@yergler.net>
2013-03-19 13:33:25 -0700

    Bumping version to 0.6-dev.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ README = open(os.path.join(here, 'README.rst')).read()
 NEWS = open(os.path.join(here, 'NEWS.txt')).read()
 
 
-version = '0.5.5'
+version = '0.6'
 
 install_requires = [
     "Sphinx",
