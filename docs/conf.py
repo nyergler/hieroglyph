cde58909d756ca827a6fd6efeb386e599bb04a42
Nathan Yergler <nathan@Nathans-MacBook-Pro.local>
2015-03-14 22:14:14 -0400

    Install sphinx_rtd_theme before attempting to build docs using it.

--- docs/conf.py
+++ docs/conf.py
@@ -125,7 +125,11 @@ slide_html_relative_path = "../"
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 if on_rtd:
+    import sphinx_rtd_theme
+
     html_theme = 'sphinx_rtd_theme'
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+
 else:
     if SPHINX_VERSION < (1, 3):
         html_theme = 'nature'
