1170224d82115046d299ae87a1808ab02311d547
Lukas Pirl <git@lukas-pirl.de>
2018-05-03 17:00:25 +0200

    hot fix in test_hieroglyph_quickstart for Sphinx >= 1.7

--- src/hieroglyph/tests/test_quickstart.py
+++ src/hieroglyph/tests/test_quickstart.py
@@ -7,7 +7,7 @@ import time
 from unittest import TestCase
 
 from six import PY2, text_type
-from sphinx import quickstart as sphinx_quickstart
+from sphinx.cmd import quickstart as sphinx_quickstart
 from sphinx.util.pycompat import execfile_
 from sphinx.util.console import nocolor, coloron
 
@@ -62,6 +62,13 @@ class TestQuickstart(TestCase):
         assert os.path.exists(conffile)
         ns = {}
         execfile_(conffile, ns)
+
+        # XXX why is there a list in the list?!
+        # Apparently introduced between Sphinx 1.6.7 and 1.7.0b1
+        if type(ns['extensions']) is list:
+            # let's make sure there is nothing we don't expect
+            self.assertEqual(len(ns['extensions']), 1)
+            ns['extensions'] = ns['extensions'][0]
 
         self.assertIn('hieroglyph', ns['extensions'])
         self.assertEqual(ns['templates_path'], ['_templates'])
